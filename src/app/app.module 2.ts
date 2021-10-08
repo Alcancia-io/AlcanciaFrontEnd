06e7af6ba080b0153b22d8431e7eb90ec708f159
Juan David Torres <juandatorr_1999@hotmail.com>
2021-10-07 20:37:18 -0500

    Update build

---
+++ src/app/app.module 2.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { AngularDelegate, IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { FormsModule } from '@angular/forms';
+
+//firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import {  AngularFireAuthModule } from "@angular/fire/compat/auth";
+
+//env
+import { environment } from 'src/environments/environment';
+
+// services
+import { AuthenticationService } from './services/authentication.service';
+
+// guards
+import { AuthGuard } from './guards/auth.guard';
+import { TokenService } from './services/token.service';
+import { AuthorizeGuard, NegateAuthorizeGuard } from './guards/authorize.guard';
+import { UniversalAppInterceptor } from './UniversalAppInterceptor';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    IonicStorageModule.forRoot({
+      name: "AlcanciaLocalStorage"
+    })
+  ],
+  providers: [
+    AuthenticationService,
+    TokenService,
+    AuthGuard,
+    AuthorizeGuard,
+    NegateAuthorizeGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: UniversalAppInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent],
+})
+
+export class AppModule {}
---
+++ src/environments/environment 2.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBCcfcDq4NLj-MC7VB_Y4AdKGTZOsQz-iQ",
+    authDomain: "alcancia.firebaseapp.com",
+    projectId: "alcancia",
+    storageBucket: "alcancia.appspot.com",
+    messagingSenderId: "142936855133",
+    appId: "1:142936855133:web:568866aa22b85d2d33b3b4",
+    measurementId: "G-SE488C3P75"
+  }
+
+};
+
+export const alcanciaBackendUrl = {
+  url: ""
+}
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
