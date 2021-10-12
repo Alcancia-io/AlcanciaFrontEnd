a8a810be9c46ad4c92afcf884139c89ebb3c060f
Srestituyo <49099635+Srestituyo@users.noreply.github.com>
2021-10-12 18:28:32 -0400

    Adding logoff button and confirm alert

--- src/app/main-screen/main-screen.page.ts
+++ src/app/main-screen/main-screen.page.ts
@@ -7,6 +7,9 @@ import { UserService } from '../services/user.service';
 import { USER_NAME } from 'src/app/guards/auth.guard';
 import { User } from '../models/user';
 import { UserModel } from '../models/userModel';
+import { AlertController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 @Component({
   selector: 'app-main-screen',
   templateUrl: './main-screen.page.html',
@@ -18,9 +21,11 @@ export class MainScreenPage implements OnInit {
 
   constructor(
     private authService: AuthenticationService,
+    private afAuth: AngularFireAuth,
     private storageService: StorageService,
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private alertController: AlertController
   ) { }
 
   ngOnInit() {
@@ -35,11 +40,34 @@ export class MainScreenPage implements OnInit {
     }
   }
   async logout() {
-    await this.authService.logout();
+    this.presentAlertConfirm();
+    
   }
 
   async getUser(){
     await this.userService.getUser().then(data => console.log('user data from the Main-screen' + data));
   }
 
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Cerrar Sesión',
+      message: '¿Estas seguro de que quieres cerrar sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+          }
+        }, {
+          text: 'Okay',
+          handler: async () => {
+            await this.authService.logout();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
