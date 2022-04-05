af09e3df39f8c712d6dfc6fdd476677dec7e2959
Juan Alcantara <j.alejandrominaya@gmail.com>
2022-04-05 17:27:32 -0500

    feat: Add interface to profile page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,18 +5,18 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBCcfcDq4NLj-MC7VB_Y4AdKGTZOsQz-iQ",
-    authDomain: "alcancia.firebaseapp.com",
-    projectId: "alcancia",
-    storageBucket: "alcancia.appspot.com",
-    messagingSenderId: "142936855133",
-    appId: "1:142936855133:web:568866aa22b85d2d33b3b4",
-    measurementId: "G-SE488C3P75"
+    apiKey: "AIzaSyBhtFQWqEbuUUBcMig1OqEsfn_hg9Dcshk",
+    authDomain: "alcancia-developers.firebaseapp.com",
+    projectId: "alcancia-developers",
+    storageBucket: "alcancia-developers.appspot.com",
+    messagingSenderId: "8144688564",
+    appId: "1:8144688564:web:c5b196c9cfaaacd4f00ee1",
+    measurementId: "G-7QXCZK66FV"
   }
 
 };
 
-export const ALCANCIA_SERVER_URL = "https://royerdonnetarenas.tech/api";
+export const ALCANCIA_SERVER_URL = "http://localhost:8000";
 export const EXCHANGE_RATE_URL = "https://v6.exchangerate-api.com/v6/460cee64dbd1d3e35cdfcc74";
 /*
  * For easier debugging in development mode, you can import the following file
